3c62ecc0386936ba71cc49fd7b49d8806b5310e7
Dominique <domizm122788@outlook.com>
2026-07-14 04:02:05 -0600

    fixed a couple more things

--- assignment2/diary.py
+++ assignment2/diary.py
@@ -5,24 +5,17 @@ def main():
     try:
         with open("diary.txt", "a") as diary_file:
             prompt_text = "What happened today? "
-        
-        while True:
-            user_input = input(prompt_text) 
-            diary_file.write(user_input + "\n")
-             # Checking for exit conditions
-            if user_input == "done for now":
-                break
-            prompt_text = "What else? "
-            
+            while True:
+                user_input = input(prompt_text)
+                diary_file.write(user_input + "\n")
+                # Checking for exit conditions
+                if user_input == "done for now":
+                    break
+                prompt_text = "What else? "
     except Exception as e:
     # Printing custom error alert
-        print(f"An exception occured: {type(e).__name__}")
-    
-        trace_back = traceback.extract_tb(e.__traceback__)
-        stack_trace = [
-            f"File : {trace[0]} , Line : {trace[1]}, Func.Name : {trace[2]}, Message : {trace[3]}"
-            for trace in trace_back
-        ]
-        print(f"Stack trace: {stack_trace}")
-    if __name__ == "__main__":
-        main()
+        print(f"An exception occured.")
+        print(type(e).__name__)
+
+if __name__ == "__main__":
+    main()
